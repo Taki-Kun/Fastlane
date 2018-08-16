822c6a12505fcc57baaca1e1ec6e5eb467b845b7
issenn <issenn@hellotalk.com>
2018-08-16 10:47:02 +0800

    Initial commit

--- fastlane/actions/upload_apk_to_fir.rb
+++ fastlane/actions/upload_apk_to_fir.rb
@@ -24,7 +24,7 @@ module Fastlane
         apk_paths = [params[:apk]] unless (apk_paths = params[:apk_paths])
         apk_paths.each do | apk |
           flavor = Actions.lane_context[SharedValues::GRADLE_FLAVOR] || /([^\/-]*)(?=-[^\/-]*\.apk$)/.match(apk)
-          change_log = "---[#{flavor}]+[#{ENV['GIT_BRANCH']}]\r\n #{get_version_name} - #{get_version_code}\r\n" + params[:change_log]
+          change_log = "---[#{flavor}]+[#{ENV['GIT_BRANCH']}]\r\n #{Actions.get_version_name} - #{Actions.get_version_code}\r\n" + params[:change_log]
           puts "Uploading APK to fir: " + apk
           puts change_log
           # Action.sh "sudo /usr/local/bin/fir p '#{apk}' -T '#{params[:app_key]}' -c '#{change_log}'"

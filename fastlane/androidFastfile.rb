171393c95381ae8ea39df1bad6a9427f9ac19eed
issenn <issenn@hellotalk.com>
2018-08-17 11:16:24 +0800

    Initial commit

--- fastlane/androidFastfile.rb
+++ fastlane/androidFastfile.rb
@@ -6,8 +6,8 @@ default_platform(:android)
 platform :android do
   before_all do |lane, options|
     # git_pull
-    # ENV['SLACK_URL'] = 'https://hooks.slack.com/services/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'
-    ENV['SLACK_URL'] = 'https://hooks.slack.com/services/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'
+    ENV['SLACK_URL'] = 'https://hooks.slack.com/services/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'
+    # ENV['SLACK_URL'] = 'https://hooks.slack.com/services/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'
     ENV['FL_SLACK_CHANNEL'] = '#devops'
     ENV['FIR_APP_TOKEN'] = '9611b6a99d280463039cbb64b7eb24ca'
     ENV["GIT_BRANCH"] = git_branch
